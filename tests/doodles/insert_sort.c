42121a3f23fed0546354089d22e03674e8edd23d
Lefr0g <amulin@student.42.fr>
2016-05-06 12:49:11 +0200

    WIP insertion sort

--- tests/doodles/insert_sort.c
+++ tests/doodles/insert_sort.c
@@ -70,14 +70,12 @@ t_list	*ft_lstinsertright(t_list *elem, t_list *pos)
 */
 int		ftls_sort_list(t_list **alst, int content_offset)
 {
-	(void)alst;
-	(void)content_offset;
-	
-	
 	t_list			*run_ptr; // list runner
 	t_list			*ref_ptr; // compared element pointer
 	t_list			*pos_ptr; // saves the pointer to the next element to be sorted
 	t_list			*head_ptr; // list head pointer
+
+	int				i;
 
 	if (!alst || !(*alst) || !(*alst)->next)
 		return (1); // exit if pointer error or single element in list
@@ -85,16 +83,21 @@ int		ftls_sort_list(t_list **alst, int content_offset)
 
 	ref_ptr = *alst;
 	head_ptr = *alst;
+	pos_ptr = NULL;
 	run_ptr = ref_ptr->next;
+	i = -1;
 	while (run_ptr)
 	{
+		ft_printf("run_ptr loop i = %d\n", ++i);
 		if (*(int*)(ref_ptr->content + content_offset) > 
 				*(int*)(run_ptr->content + content_offset))
 		{
+			ft_printf("Check 0\n");
 			pos_ptr = run_ptr->next; // sauvegarde de la position pour reprise
 			// detacher l'element
 			if (!ft_lstdetachone(run_ptr))
 				return (0);
+			ft_printf("Check : (elem detached)\n");
 
 			// lancer le parcour vers la gauche
 			while (ref_ptr)
@@ -105,20 +108,30 @@ int		ftls_sort_list(t_list **alst, int content_offset)
 				{
 					ft_lstinsertright(run_ptr, ref_ptr);
 					ref_ptr = NULL;
+					ft_printf("Check : (elem inserted)\n");
 				}
 				else
 				{
 					// inserer en tete de liste si aucune position ne convient
 					if (!(ref_ptr = ref_ptr->prev))
+					{
 						ft_lstappend(&head_ptr, run_ptr);
+						ft_printf("Check : (elem appended)\n");
+					}
 				}
 			}
+			ft_printf("Check 1\n");
 			// on se repositionne au bout de la liste d'elements deja tries
-			run_ptr = pos_ptr;
-			ref_ptr = run_ptr->prev;
+			if ((run_ptr = pos_ptr))
+				ref_ptr = run_ptr->prev;
+			else
+				ref_ptr = NULL;
+			ft_printf("Check 2\n");
+			sleep(1);
 		}
 		else
 		{
+			ft_printf("Check : nothing to switch, moving on\n");
 			run_ptr = run_ptr->next;
 			ref_ptr = ref_ptr->next;
 		}
